bc0dab4e70fa4f175ff15d150f100f9d500e455a
albertharjanto <71241075@students.ukdw.ac.id>
2025-02-08 19:23:54 +0700

    nilaitukarusd

---
+++ nilaitukarusd.py
@@ -0,0 +1,11 @@
+# nilai kurs 1 US$ ke IDR
+kursusd = 16330
+# informasi program
+print('Program konversi US$ ke IDR')
+print('Kurs saat ini 1 US$ = ', kursusd, 'Rupiah')
+# input jumlah US$ yang mau ditukar
+jumlahusd = float(input('Masukkan jumlah uang yang mau ditukar ke Rupiah: '))
+# hitung nilainya dalam Rupiah
+dalamrupiah = jumlahusd * kursusd
+# tampilkan hasilnya
+print('Hasil konversi = Rp. ', dalamrupiah)
